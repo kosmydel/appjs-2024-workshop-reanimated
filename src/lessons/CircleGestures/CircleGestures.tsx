8724435ad456ce3b140ebdb21be38c507dcb3349
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2024-08-14 09:52:39 +0200

    lab1

--- src/lessons/CircleGestures/CircleGestures.tsx
+++ src/lessons/CircleGestures/CircleGestures.tsx
@@ -2,12 +2,66 @@ import { Container } from "@/components/Container";
 import { hitSlop } from "@/lib/reanimated";
 import { colorShades, layout } from "@/lib/theme";
 import { StyleSheet, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 export function CircleGesturesLesson() {
+  const isInteracting = useSharedValue(false);
+  const scale = useDerivedValue(() => {
+    return withSpring(isInteracting.value ? 2 : 1);
+  });
+
+  const x = useSharedValue(0);
+
+  const panGesture = Gesture.Pan()
+    .onBegin(() => {
+      isInteracting.value = true;
+    })
+    .onChange((ev) => {
+      x.value += ev.changeX;
+    })
+    .onFinalize(() => {
+      isInteracting.value = false;
+    })
+    .onEnd(() => {
+      x.value = withSpring(0);
+    });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      borderWidth: interpolate(
+        scale.value,
+        [1, 2],
+        [layout.knobSize / 2, 2],
+        Extrapolation.CLAMP
+      ),
+      transform: [
+        { translateX: x.value },
+        {
+          scale: scale.value,
+        },
+      ],
+    };
+  });
+
+  const gestures = Gesture.Simultaneous(panGesture);
+
   return (
     <Container>
       <View style={{ flex: 1, justifyContent: "center" }}>
-        <View style={[styles.knob]} hitSlop={hitSlop} />
+        <GestureDetector gesture={gestures}>
+          <Animated.View
+            style={[styles.knob, animatedStyle]}
+            hitSlop={hitSlop}
+          />
+        </GestureDetector>
       </View>
     </Container>
   );
